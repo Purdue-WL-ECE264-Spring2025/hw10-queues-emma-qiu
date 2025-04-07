f249833e58788b3e145d8be9df803291977036b1
Emma Xinliang Qiu <qiu246@ececomp2.ecn.purdue.edu>
2025-04-06 21:43:39 -0400

    saved hw10

--- linked_list.c
+++ linked_list.c
@@ -1,18 +1,81 @@
 #include "linked_list.h"
-
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) {
+  struct list_node *node = malloc(sizeof(struct list_node));
+  if (node == NULL) {
+    return NULL;
+  }
+  node->value = value;
+  node->next = NULL;
+  return node;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);
+  if (node == NULL) {
+    return;
+  }
+  node->next = list->head;
+  list->head = node;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *node = new_node(value);
+  if (node == NULL) {
+    return;
+  }
+  if (list->head == NULL) {
+    list->head = node;
+    return;
+  }
+  struct list_node *cur = list->head;
+  while (cur->next != NULL) {
+    cur = cur->next;
+  }
+  cur->next = node;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) {
+  if (list->head == NULL) {
+    return 0;
+  }
+  struct list_node *node = list->head;
+  list->head = node->next;
+  size_t value = node->value;
+  free(node);
+  return value;
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) {
+    if (list->head == NULL) {
+    return 0;
+  }
+  if (list->head->next == NULL) {
+    size_t value = list->head->value;
+    free(list->head);
+    list->head = NULL;
+    return value;
+  }
+  struct list_node *cur = list->head;
+  while (cur->next->next != NULL) {
+    cur = cur->next;
+  }
+  size_t value = cur->next->value;
+  free(cur->next);
+  cur->next = NULL;
+  return value;
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) {
+  struct list_node *cur = list.head;
+  while (cur != NULL) {
+    struct list_node *next = cur->next;
+    free(cur);
+    cur = next;
+  }
+  list.head = NULL;
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,103 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    uint64_t serialized_state = serialize(state);
+    insert_at_head(&q->data, serialized_state);
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {
+    if (q->data.head == NULL) {
+        return(struct game_state){0};
+    }
+    uint64_t serialized_state = remove_from_tail(&q->data);
+    return deserialize(serialized_state);
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+bool gs_solved(struct game_state* state) {
+    static const uint8_t final_tiles[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 0}
+    };
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (state->tiles[i][j] != final_tiles[i][j]) {
+                return false;
+            }
+        }
+    }
+    return state->empty_row == 3 && state->empty_col == 3;
+}
+
+bool state_in_list(struct queue q, struct game_state curr) {
+    struct list_node *cur = q.data.head;
+    uint64_t curr_hash = serialize(curr);
+    while (cur != NULL) {
+        if (cur->value == curr_hash) {
+            return true;
+        }
+        cur = cur->next;
+    }
+    return false;
+}
+
+int number_of_moves(struct game_state start) {
+    if (gs_solved(&start)) {
+        return 0;
+    }
+    struct queue q = {0};
+    struct queue visited = {0};
+
+    enqueue(&q, start);
+    enqueue(&visited, start);
+    while (q.data.head != NULL) {
+        struct game_state curr = dequeue(&q);
+        if (gs_solved(&curr)) {
+            free_list(q.data);
+            free_list(visited.data);
+            return curr.num_steps;
+        }
+           
+        //move up
+        struct game_state next = curr;
+        move_up(&next);
+        // check if the state has been visited
+        if (!state_in_list(visited, next)) {
+            enqueue(&q, next);
+            enqueue(&visited, next);
+        }
+
+        //move down
+        next = curr;
+        move_down(&next);
+        // check if the state has been visited
+        if (!state_in_list(visited, next)) {
+            enqueue(&q, next);
+            enqueue(&visited, next);
+        }
+
+        //move left
+        next = curr;
+        move_left(&next);
+        // check if the state has been visited
+        if (!state_in_list(visited, next)) {
+            enqueue(&q, next);
+            enqueue(&visited, next);
+        }
+
+        //move right
+        next = curr;
+        move_right(&next);
+        // check if the state has been visited
+        if (!state_in_list(visited, next)) {
+            enqueue(&q, next);
+            enqueue(&visited, next);
+        }
+    }
+    free_list(q.data);
+    free_list(visited.data);
+    return 0;
+}
